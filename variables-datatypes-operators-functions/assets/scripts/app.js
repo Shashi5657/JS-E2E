72836a912336d907297e61eced5be9c2fea21c5a
Shashi5657 <sangepushashidhar@gmail.com>
2024-08-14 22:57:23 +0530

    variables, operators, data types

--- variables-datatypes-operators-functions/assets/scripts/app.js
+++ variables-datatypes-operators-functions/assets/scripts/app.js
@@ -1,16 +1,26 @@
-// alert("This is from the app.js file");
-const defaultResult = 0;
+// // alert("This is from the app.js file");
+// const defaultResult = 0;
+// let currentResult = defaultResult;
+
+// function addNumbers(num1, num2) {
+//   const result = num1 + num2;
+//   return result;
+// }
+
+// addNumbers(10, 12);
+
+// currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+// let calculationDescription = `(${defaultResult} + 10) * 3/2 - 1`;
+
+// outputResult(currentResult, calculationDescription);
+
+let defaultResult = 0;
 let currentResult = defaultResult;
 
-function addNumbers(num1, num2) {
-  const result = num1 + num2;
-  return result;
+function add() {
+  currentResult += userInput.value;
+  outputResult(currentResult, "");
 }
 
-addNumbers(10, 12);
-
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
-
-let calculationDescription = `(${defaultResult} + 10) * 3/2 - 1`;
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener("click", add);
